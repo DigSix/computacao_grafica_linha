68fa042e3d907671414ed46c51eb9d578c066276
DigSix <thediogoviana@gmail.com>
2026-08-21 08:59:58 -0300

    aula 19.08.26: relembrando pitagoras aprendendo a desnhar com angulos

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,27 @@
 #include <iostream>
 #include <GL/freeglut.h>
+#include <cmath>
+
+
+
+#define PI 3.14159
+
+#define WWIDTH  500
+#define WHEIGHT 500
+
 
 void display();
 void keyboard(unsigned char key, int x, int y);
+double degrees_to_radians(double degrees);
 
 int main(int argc, char** argv) {
 	glutInit(&argc, argv);
 	
-	glutInitWindowSize(500, 250);
+	glutInitWindowSize(WWIDTH, WHEIGHT);
 
-	glutCreateWindow("Desenhando uma linha");
-	glClearColor(0.20, 0.20, 0.22, 1.0);
-	glOrtho(0, 256, 0, 256, -1, 1);
+	glutCreateWindow("Criando poligonos");
+	glClearColor(1.0, 1.0, 1.0, 1.0);
+	glOrtho(0, WWIDTH, 0, WHEIGHT, -1, 1);
 
 	glutDisplayFunc(display);
 	glutKeyboardFunc(keyboard);
@@ -24,11 +34,30 @@ int main(int argc, char** argv) {
 
 void display(void) {
 	glClear(GL_COLOR_BUFFER_BIT);
-	glColor3f(1.0, 0.0, 0.0);
 
 	glBegin(GL_LINES);
-	glVertex2i(10, 10);
-	glVertex2i(245, 245);
+
+	int    sides  = 5;
+	float  radius = 30;
+	float  x   	  = WWIDTH  / 2 - radius / 2;
+	float  y   	  = WHEIGHT / 2 - radius / 2;
+	float  angle  = 360.0f / sides;
+	double de     = 0 ;
+
+	for(int i = 0; i < sides; i++){
+
+		glColor3f(1.0, 0.0, 1.0);
+
+		glVertex2f(x, y);
+
+		x  += radius * std::cos(degrees_to_radians(de));
+		y  += radius * std::sin(degrees_to_radians(de));
+		de += angle;
+		
+		glVertex2f(x, y);
+
+	}
+
 	glEnd();
 
 	glFlush();
@@ -40,4 +69,9 @@ void keyboard(unsigned char key, int x, int y) {
 		exit(0);
 		break;
 	}
+}
+
+
+double degrees_to_radians(double degrees) {
+	return degrees * (PI / 180.0);
 }
